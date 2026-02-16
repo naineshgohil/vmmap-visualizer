c5797406773e6fb02aedd29cbccb1759964764d5
Nainesh Gohil <naineshg93@gmail.com>
2026-02-16 09:55:24 -0500

    Switch ObjC bridge to RCTEventEmitter with snapshot callback
    Extend RCTEventEmitter, register onSnapshotCallback with Zig create,
    build NSDictionary from CSnapshot and emit onSnapshot events to JS.

--- frontend/macos/VmmapVisualizer-macOS/VmmapCollectorModule.m
+++ frontend/macos/VmmapVisualizer-macOS/VmmapCollectorModule.m
@@ -6,15 +6,31 @@
 //
 
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 #import <dlfcn.h>
 
 typedef struct {
   bool ok;
   const char *error_msg;
+  void* ref;
 } Result;
 
+typedef struct {
+     const char *region_type;
+     uint64_t start_addr;
+     uint64_t end_addr;
+     uint64_t virtual_size;
+     uint64_t resident_size;
+ } CRegion;
+
+ typedef struct {
+     int64_t timestamp_ms;
+     CRegion *regions;
+     unsigned int region_count;
+ } CSnapshot;
+
 // Function pointers to Zig dylib
-typedef void* (*CreateFn)(int, unsigned int);
+typedef Result (*CreateFn)(int, unsigned int, void (*)(const CSnapshot *));
 typedef void (*DestroyFn)(void*);
 typedef Result (*StartFn)(void*);
 typedef void (*StopFn)(void*);
@@ -22,7 +38,7 @@ typedef unsigned int (*CountFn)(void*);
 typedef char* (*GetJsonFn)(void*);
 typedef void (*FreeStringFn)(char*);
 
-@interface VmmapCollectorModule : NSObject <RCTBridgeModule>
+@interface VmmapCollectorModule : RCTEventEmitter <RCTBridgeModule>
 @property (nonatomic) void* collector;
 @property (nonatomic) void* libHandle;
 @property (nonatomic) CreateFn createFn;
@@ -36,11 +52,38 @@ typedef void (*FreeStringFn)(char*);
 
 @implementation VmmapCollectorModule
 
-RCT_EXPORT_MODULE();
+RCT_EXPORT_MODULE()
+
+static VmmapCollectorModule *_sharedInstance = nil;
+
+void onSnapshotCallback(const CSnapshot *snapshot) {
+  NSMutableArray *regions = [NSMutableArray arrayWithCapacity:snapshot->region_count];
+  
+  for (unsigned int i = 0; i < snapshot->region_count; i++) {
+    CRegion r = snapshot->regions[i];
+    [regions addObject:@{
+      @"type": [NSString stringWithUTF8String:r.region_type],
+      @"start": @(r.start_addr),
+      @"end": @(r.end_addr),
+      @"vsize": @(r.virtual_size),
+      @"rsize": @(r.resident_size),
+    }];
+  }
+  
+  NSDictionary *dict = @{
+    @"timestamp_ms": @(snapshot->timestamp_ms),
+    @"regions": regions,
+  };
+  
+  [_sharedInstance sendEventWithName:@"onSnapshot" body:dict];
+}
+
 
 - (instancetype)init {
   self = [super init];
   if (self) {
+    _sharedInstance = self;
+    
     NSString *libPath = [[NSBundle mainBundle] pathForResource:@"libvmmap_collector" ofType:@"dylib"];
     _libHandle = dlopen([libPath UTF8String], RTLD_NOW);
     
@@ -58,17 +101,22 @@ RCT_EXPORT_MODULE();
   return self;
 }
 
+-(NSArray<NSString *> *)supportedEvents {
+  return @[@"onSnapshot"];
+}
+
 RCT_EXPORT_METHOD(create:(int)pid interval:(int)intervalMs resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
   if (_collector) {
     _destroyFn(_collector);
   }
   
-  _collector = _createFn(pid, (unsigned int) intervalMs);
+  Result r = _createFn(pid, (unsigned int) intervalMs, &onSnapshotCallback);
   
-  if (_collector) {
+  if (r.ok) {
+    _collector = r.ref;
     resolve(@"");
   } else {
-    reject(@"NO_DATA", @"Collector not initialized", nil);
+    reject(@"CREATE_FAILED", [NSString stringWithUTF8String:r.error_msg], nil);
   }
 }
 
